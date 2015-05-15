3b7c3655a032bd4adb0a5ff09c307f80fb8204dd
palver123 <palver123@gmail.com>
2015-05-16 00:14:58 +0200

    oops

---
+++ WololoGame/Logic/LogicObjectBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WololoGame.Logic
+{
+    public abstract class LogicObjectBase : ILogicObjectForPhysics
+    {
+        public abstract void CollidedWith(ILogicObjectForPhysics otherObject);
+
+        public bool IsDeadly()
+        {
+            return false;
+        }
+
+        
+    }
+}
---
+++ WololoGame/Logic/PlayerLogic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WololoGame.Logic
+{
+    public class PlayerLogic : LogicObjectBase
+    {
+        public override void CollidedWith(ILogicObjectForPhysics otherObject)
+        {
+            var logicObject = otherObject as LogicObjectBase;
+            if (logicObject != null)
+            {
+                if (logicObject.IsDeadly())
+                    Logger.Get().Log("main", LogLevel.error, "Halál");
+            }
+        }
+    }
+}
