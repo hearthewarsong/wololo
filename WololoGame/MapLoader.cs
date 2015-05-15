532c23d267eca0a92c9811d94a79e7e99564a100
hearthewarsong <hearthewarsong@gmail.com>
2015-05-16 00:29:03 +0200

    fvdfsfsdf

--- WololoGame/MapLoader.cs
+++ WololoGame/MapLoader.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace WololoGame
@@ -12,9 +13,26 @@ namespace WololoGame
     {
         public void LoadMap(Game1 game, string mapfile)
         {
-            //using (StreamReader reader = new StreamReader(mapfile))
+            using (StreamReader levelFileStream = new StreamReader(TitleContainer.OpenStream("Content/maps/begining.txt")))
             {
+                string line;
+                while ((line = levelFileStream.ReadLine())!= null)
+                {
+                    string[] fields = Regex.Split(line, "\\s+");
 
+                    if (fields.Length > 1)
+                    {
+                        if (fields[0].ToLower() == "player")
+                        {
+
+                           
+                        }
+                        else if (fields[0].ToLower() == "player")
+                        {
+
+                        }
+                    }
+                }
             }
         }
     }
